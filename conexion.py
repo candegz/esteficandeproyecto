10b64151dd342f934cc015f700abd4e8bdc7d8ca
candegz <candevguzman@gmail.com>
2024-02-16 00:09:52 -0300

    conexion

--- conexion.py
+++ conexion.py
@@ -1,1 +1,30 @@
 import mysql.connector
+
+
+    def conexion(self):
+        db = mysql.connector.connect(host = "localhost" , user = "root" , passwd = "", database = "Ta Te Ti")
+        return db
+    
+    def gamerone(self, user1):
+        con = self.conexion()
+        cursor = con.cursor()
+        consul = "INSERT INTO user (nom_usu, id_tip_usu) VALUES (%s, %s)"
+        cursor.execute(consul, user1)
+        con.commit()
+        con.close()
+
+    def gamertwo(self, user2):
+        con = self.conexion()
+        cursor = con.cursor()
+        consul = "INSERT INTO user (nom_usu, id_tip_usu) VALUES (%s, %s)"
+        cursor.execute(consul, user2)
+        con.commit()
+        con.close()
+
+    def Partida(self, gamers):
+        con = self.conexion()
+        cursor = con.cursor()
+        consul = "INSERT INTO partida (id_user1, id_user2) VALUES (%s, %s)"
+        cursor.execute(consul,gamers)
+        con.commit()
+        con.close()
